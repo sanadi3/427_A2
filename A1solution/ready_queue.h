b5cd06a04b8dda97ec5cd93b91873028c0b395a6
nnnigel <166702176+nnnigel@users.noreply.github.com>
2026-02-22 02:36:27 -0500

    Add new functions for SJF policy and queue management

--- A1solution/ready_queue.h
+++ A1solution/ready_queue.h
@@ -6,6 +6,10 @@
 void ready_queue_add_to_tail(PCB *p);
 void ready_queue_add_to_head(PCB *p); // Helper for some policies
 PCB* ready_queue_pop_head();
+PCB* ready_queue_pop_shortest(); // Helper for SJF policy
+void ready_queue_insert_sorted(PCB *p); // insert PCB into the ready queue keeping it sorted by job_length_score (ascending)
+void ready_queue_age_all(); // age by decreasing job_length_score by 1
+PCB* ready_queue_peek_head(); // look at the head of the ready queue 
 void ready_queue_print(); // Helper for debugging
 
 #endif
