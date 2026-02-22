8faf3df17096291500d1db4e1b18621ca21ee43a
nnnigel <166702176+nnnigel@users.noreply.github.com>
2026-02-22 02:35:30 -0500

    Add job_length_score to PCB structure

--- A1solution/pcb.h
+++ A1solution/pcb.h
@@ -7,10 +7,10 @@ typedef struct PCB {
     int end;
     int pc; // Program Counter
     int job_time; // Total time required for the job
+    int job_length_score; // Score used for SJF with aging
     struct PCB *next; // Pointer to the next PCB in the queue
 } PCB;
 
 PCB* make_pcb(int start, int end);
 
 #endif
-
